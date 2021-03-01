07a867aa308b61d825e14fcaf253d9ed5fe5e23d
Kim Jae Eun <kj6659@naver.com>
2021-03-01 23:17:06 +0900

    5

--- 20210226/test2.R
+++ 20210226/test2.R
@@ -3,7 +3,8 @@ test2 <- scan("data/iotest2.txt", what="",encoding="UTF-8") ;test2
 r <- names(which.max(table(test2)))
 cat("가장 많이 등장한 단어는",r,"입니다.")
 
-max(table(test2))
+
 test2
 table(test2)
 which.max(table(test2))
+max(table(test2))
