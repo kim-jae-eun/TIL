2fb65359ace116c39aefb53ed797a497c66b9bb4
Kim Jae Eun <kj6659@naver.com>
2021-03-05 23:42:38 +0900

    1

---
+++ 20210305/lab9.R
@@ -0,0 +1,72 @@
+# [ OPEN API 실습 1 ]
+library(XML)
+library(httr)
+searchUrl<- "https://openapi.naver.com/v1/search/blog.xml"
+Client_ID <- "izGsqP2exeThwwEUVU3x"
+Client_Secret <- "WrwbQ1l6ZI"
+
+query <- URLencode(iconv("맛집","euc-kr","UTF-8"))
+url <- paste0(searchUrl, "?query=", query, "&display=100")
+doc <- GET(url, add_headers("Content_Type" = "application/xml",
+                            "X-Naver-client-Id" = Client_ID, "X-naver-Client-Secret" = Client_Secret))
+
+doc2 <- htmlParse(doc, encoding="UTF-8")
+text<- xpathSApply(doc2, "//item/description", xmlValue)
+text <- gsub("</?b>", "", text)
+write(text, "data/naverblog.txt")
+
+
+
+# [ OPEN API 실습 2 ]
+library(rtweet)
+appname <- "edu_data_collection"
+api_key <- "RvnZeIl8ra88reu8fm23m0bST"
+api_secret <- "wTRylK94GK2KmhZUnqXonDaIszwAsS6VPvpSsIo6EX5GQLtzQo"
+access_token <- "959614462004117506-dkWyZaO8Bz3ZXh73rspWfc1sQz0EnDU"
+access_token_secret <- "rxDWfg7uz1yXMTDwijz0x90yWhDAnmOM15R6IgC8kmtTe"
+twitter_token <- create_token(
+  app = appname,
+  consumer_key = api_key,
+  consumer_secret = api_secret,
+  access_token = access_token,
+  access_secret = access_token_secret)
+
+key <- "코로나"
+key <- enc2utf8(key)
+result <- search_tweets(key, n=100, token = twitter_token)
+text <- gsub("[[:cntrl:][:punct:](A-z)(https://t.co/4PZ5mARDgK)]","",result$text)
+write(text, "data/twitter.txt")
+
+
+
+# [ OPEN API 실습 3 ]
+library(XML)
+API_key  <- "%2BjzsSyNtwmcqxUsGnflvs3rW2oceFvhHR8AFkM3ao%2Fw50hwHXgGyPVutXw04uAXvrkoWgkoScvvhlH7jgD4%2FRQ%3D%3D"
+bus_No <- "360"
+url <- paste("http://ws.bus.go.kr/api/rest/busRouteInfo/getBusRouteList?ServiceKey=", API_key, "&strSrch=", bus_No, sep="")
+doc <- xmlParse(url, encoding="UTF-8")
+top <- xmlRoot(doc)
+df <- xmlToDataFrame(getNodeSet(doc, "//itemList"))
+#View(df)
+cat("[360번 버스정보]","\n노선ID : ",df[[1]][1],"\n노선길이 : ", df[[10]][1],
+    "\n기점 : ", df[[12]][1], "\n종점 : ", df[[4]][1],
+    "\n배차간격 : ", df[[13]][1])
+
+
+
+# [ OPEN API 실습 4 ]
+library(jsonlite)
+searchUrl<- "https://openapi.naver.com/v1/search/news.json"
+Client_ID <- "izGsqP2exeThwwEUVU3x"
+Client_Secret <- "WrwbQ1l6ZI"
+query <- URLencode(iconv("빅데이터","euc-kr","UTF-8"))
+url <- paste0(searchUrl, "?query=", query, "&display=100")
+doc <- GET(url, add_headers("Content_Type" = "application/json",
+                            "X-Naver-client-Id" = Client_ID, "X-naver-Client-Secret" = Client_Secret))
+json_data <- content(doc, type = 'text', encoding = "UTF-8")
+json_obj <- fromJSON(json_data)
+df <- data.frame(json_obj)
+text <- df[["items.title"]]
+text <- gsub("</?b>", "", text)
+text <- gsub("&.+t;", "", text)
+write(text, "data/navernews.txt")
